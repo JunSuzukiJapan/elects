26a8e6e50d66ce69dba2d1b036e064a8d490f2ac
Jun Suzuki <jun.suzuki.japan@gmail.com>
2016-01-07 00:30:47 +0900

    不要なコメントの削除

--- examples/documents/src/html/ts/main.tsx
+++ examples/documents/src/html/ts/main.tsx
@@ -4,13 +4,8 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 const path = require('path');
 
-//const Markdown: typeof React.Component = require('./js/markdown').Markdown;
-//const filename = path.resolve(__dirname + '/md/index.md');
-//const element = React.createElement(Markdown, {path: filename});
-
 const MainBody: typeof React.Component = require('./js/main-body').MainBody;
 const element = React.createElement(MainBody);
-
 const container = document.getElementById('container');
 
 ReactDOM.render(element, container);
